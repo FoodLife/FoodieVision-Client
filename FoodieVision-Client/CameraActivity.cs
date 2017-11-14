e792a1b08fa92f6ebd627eefab308e7b526d06ae
Clayton Horstman <horstman.22@wright.edu>
2017-11-14 17:29:43 -0500

    Added  basic structure for camera features

--- FoodieVision-Client/CameraActivity.cs
+++ FoodieVision-Client/CameraActivity.cs
@@ -11,29 +11,115 @@ using System.Collections.Generic;
 using Android.Content.PM;
 using Android.Provider;
 
+using Java.IO;
+using Android.Graphics;
+using System.Diagnostics.Contracts;
+
 namespace FoodieVisionClient
 {
-    [Activity(Label = "Camera", Theme = "@style/android:Theme.Holo.Light.NoActionBar")]
+    
+    using System;
+    using System.Collections.Generic;
+    using Android.App;
+    using Android.Content;
+    using Android.Content.PM;
+    using Android.Graphics;
+    using Android.OS;
+    using Android.Provider;
+    using Android.Widget;
+    using Java.IO;
+    using Environment = Android.OS.Environment;
+    using Uri = Android.Net.Uri;
+
+    public static class App
+    {
+        public static File _file;
+        public static File _dir;
+        public static Bitmap bitmap;
+    }
+
+    [Activity(Label = "Camera App Demo")]
     public class CameraActivity : Activity
     {
+
+        private ImageView _imageView;
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            // Make it available in the gallery
+
+            Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            Uri contentUri = Uri.FromFile(App._file);
+            mediaScanIntent.SetData(contentUri);
+            SendBroadcast(mediaScanIntent);
+
+            // Display in ImageView. We will resize the bitmap to fit the display
+            // Loading the full sized image will consume to much memory 
+            // and cause the application to crash.
+
+            int height = Resources.DisplayMetrics.HeightPixels;
+            int width = _imageView.Height;
+            App.bitmap = App._file.Path.LoadAndResizeBitmap(width, height);
+            if (App.bitmap != null)
+            {
+                _imageView.SetImageBitmap(App.bitmap);
+                App.bitmap = null;
+            }
+
+            // Dispose of the Java side bitmap.
+            GC.Collect();
+        }
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.MainLayout);
+            if (IsThereAnAppToTakePictures())
+            {
+                CreateDirectoryForPictures();
 
-            //Initializing button from layout
-            Button login = FindViewById<Button>(Resource.Id.CameraButton);
+                Button button = FindViewById<Button>(Resource.Id.myButton);
+                _imageView = FindViewById<ImageView>(Resource.Id.imageView1);
 
-            //Login button click action
-            login.Click += (object sender, EventArgs e) => {
+                //button.Click += TakeAPicture; 
+                TakeAPicture();
+            }
 
-                Android.Widget.Toast.MakeText(this, "Camera Button Clicked!", Android.Widget.ToastLength.Short).Show();
-                StartActivity(typeof(CameraActivity));
-
-            };
         }
 
+
+
+        private void CreateDirectoryForPictures()
+        {
+            App._dir = new File(
+                Environment.GetExternalStoragePublicDirectory(
+                    Environment.DirectoryPictures), "FoodieVision");
+            if (!App._dir.Exists())
+            {
+                App._dir.Mkdirs();
+            }
+        }
+
+        private bool IsThereAnAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities =
+                PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+
+        private void TakeAPicture(object sender, EventArgs eventArgs)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+
+            App._file = new File(App._dir, String.Format("myPhoto_{0}.jpg", Guid.NewGuid()));
+
+            intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(App._file));
+
+            StartActivityForResult(intent, 0);
+        }
     }
 }
