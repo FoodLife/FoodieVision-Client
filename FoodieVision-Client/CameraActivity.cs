be73c83ebf3aea4730d9ea3263ea88d749d6b638
Clayton Horstman <horstman.22@wright.edu>
2017-11-14 15:54:47 -0500

    init

---
+++ FoodieVision-Client/CameraActivity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Net.Http;
+using System.IO;
+using Android.Graphics;
+
+namespace FoodieVision-Client
+{
+    [Activity(Label = "camera", MainLauncher = true, Theme = "@style/android:Theme.Holo.Light.NoActionBar")]
+    public class CameraActivity : Activity
+    {
+     
+    }
+}
---
+++ FoodieVision-Client/LoginActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Net.Http;
+
+namespace FoodieVision-Client
+{
+    [Activity(Label = "POIApplication", MainLauncher = true, Theme = "@style/android:Theme.Holo.Light.NoActionBar")]
+    public class LoginActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Login);
+
+            //Initializing button from layout
+            Button login = FindViewById<Button>(Resource.Id.login);
+
+            //Login button click action
+            login.Click += (object sender, EventArgs e) => {
+                
+                Android.Widget.Toast.MakeText(this, "Login Button Clicked!", Android.Widget.ToastLength.Short).Show();
+                StartActivity(typeof(MainActivity));
+
+            };
+        }
+    }
+}
